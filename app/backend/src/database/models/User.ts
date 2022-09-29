630273951cb24a08e0dacfe5e1490b29a6f26c5c
Pedro Reis <pedronr0309@gmail.com>
2022-09-28 22:10:42 -0300

    feat: create user model

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,45 @@
+import { STRING } from 'sequelize';
+import { INTEGER } from 'sequelize';
+import { Model } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  public id!: number;
+  public username!: string;
+  public role!: string;
+  public email!: string;
+  public password!: string;
+}
+
+User.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  username: {
+    allowNull: false,
+    type: STRING,
+    unique: true
+  },
+  role: {
+    allowNull: false,
+    type: STRING,
+  },
+  email: {
+    allowNull: false,
+    type: STRING,
+    unique: true
+  },
+  password: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+});
+
+export default User;
